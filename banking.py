817e866c5baa22f396d598b3a267c2704e2fd4fe
laylaheath <heathlayla@icloud.com>
2025-04-12 10:39:22 -0500

    Required Assignment Files

--- banking.py
+++ banking.py
@@ -1,4 +1,14 @@
 class Banking:
     '''
-    Finish later
+    Field needed:
+    -first name
+    -last name
+    -account number (an 8-digit code starting with 2 letters)
+    -4 actions
+        - deposit
+        - withdraw
+        - balance
+        - create new account
+    - while loop that runs the program until exit word is entered
+
     '''
