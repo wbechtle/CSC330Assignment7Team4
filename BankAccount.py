71556da4f8ce74910b6c96755ee4f86784c0f021
stenceld <stenceld@csp.edu>
2025-04-27 19:50:26 -0500

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -1,7 +1,5 @@
 import random
 
-# Dictionary to keep track of current accounts
-# currAccounts = {}
 
 class BankAccount(): # removed parameters - LH
     allAccounts = []
@@ -33,28 +31,6 @@ class BankAccount(): # removed parameters - LH
             BankAccount.currNum += 1
             return account_num
 
-        ''' Generate account with first two letters are the first intial of the first name
-        and first initial of the last name. Then followed by six random numbers. '''
-        '''
-        prefix = self.__firstName[0].upper() + self.__lastName[0].upper()
-
-        # Source: https://www.geeksforgeeks.org/python-generate-random-string-of-given-length/
-        randomNums = ''.join(str(random.randint(0, 9)) for _ in range(6))
-
-        accountNum = prefix + randomNums
-
-        # Check and see if account num already exists
-        for acc in currAccounts:
-            if acc['Account'] == accountNum:
-                return generateAccount(self)
-                '''
-        ''' If it passes through the for loop then it is unique and can be appended
-        to the currAccounts dictionary. ''''''
-        currAccounts.append({
-            'name': f"{self.__firstName} {self.__lastName}",
-            'account': self.__accountNum
-            
-        })'''
 
     # -------- Getters --------
     def get_firstName(self):
