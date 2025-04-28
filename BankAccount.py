170757e1351da5dd78ab1782bef32a15c1fedf1b
laylaheath <heathlayla@icloud.com>
2025-04-27 20:04:05 -0500

    Required Assignment Files

--- BankAccount.py
+++ BankAccount.py
@@ -1,30 +1,38 @@
-# Dominic Stencel
+# Dom Stencel
 # CSC 330 100
 # Session 7
 # Final Project - BankAccount Class
-# 4/12/2025
+# 4/27/25
 
+############################################
+#####           BANKACCOUNT         ########
+############################################
+class BankAccount():
+    allAccounts = [] # list of all BankAccount objects
+    accountNums = [] # list of all account numbers
+    currNum = 100000 # first number used for account
 
-class BankAccount(): # removed parameters - LH
-    allAccounts = []
-    accountNums = []
-    currNum = 100000
-
+#############################################
+#########   Bank account constructor ########
+#############################################
     def __init__(self, firstName, lastName) : # Removed account num and balance parameters - LH
+
         self.__firstName = firstName
         self.__lastName = lastName
-        self.__accountNum = self.generateAccount() # removed parameters - LH
+        self.__accountNum = self.generateAccount()
         self.__balance = 0.0
 
     def __str__ (self): # removed parameters
         return f'{self.__firstName} {self.__lastName} account: #{self.__accountNum} \nBalance: {self.__balance}'
 
-    # -------- Generate Account --------
+###############################################################
+#####   generateAccount() - creates new account number ########
+###############################################################
     def generateAccount(self):
 
-        first = self.__firstName[0].upper()
-        last = self.__lastName[0].upper()
-        num_part = f'{BankAccount.currNum}'
+        first = self.__firstName[0].upper() # first letter of first name
+        last = self.__lastName[0].upper() # first letter of last name
+        num_part = f'{BankAccount.currNum}' # uses number for number part of account number
         #randomNums = ''.join(str(random.randint(0, 9)) for _ in range(6))
         account_num = first + last + num_part
 
@@ -35,44 +43,50 @@ class BankAccount(): # removed parameters - LH
             BankAccount.currNum += 1
             return account_num
 
-
-    # -------- Getters --------
+#########################################
+#######         GETTERS         #########
+#########################################
     def get_firstName(self):
-        return self.__firstName
+        return self.__firstName # get first name
 
     def get_lastName(self):
-        return self.__lastName
+        return self.__lastName # get last name
 
     def get_accountNumber(self):
-        return self.__accountNum
+        return self.__accountNum # get account Number
 
     def get_balance(self):
-        return self.__balance
-
-
-    # -------- Setters --------
+        return self.__balance # get balance
+########################################
+######           SETTERS        ########
+########################################
     def set_firstName(self, first_name):
-        self.__firstName = first_name
+        self.__firstName = first_name # set first name
 
     def set_lastName(self, last_name):
-        self.__lastName = last_name
+        self.__lastName = last_name # set last name
 
     def set_balance(self, amount):
-        if amount >= 0:
+        if amount >= 0: # set positive balance
             self.__balance = amount
         else:
             raise ValueError("Balance cannot be negative.")
 
-    # -------- Deposit & Withdraw --------
+    ###############################################################
+    ######     deposit() - deposit money to an account     ########
+    ###############################################################
     def deposit(self, amount):
         if amount > 0:
-            self.__balance += amount
+            self.__balance += amount # add money to account if amount is positive
             return self.__balance
         else:
             raise ValueError("Deposit must be positive.")
 
+    ###############################################################
+    ######      withdraw() - withdraw from an account      ########
+    ###############################################################
     def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
+        if 0 < amount <= self.__balance: # withdraws amount, so long as amount is positive and less than balance
             self.__balance -= amount
             return self.__balance
         else:
